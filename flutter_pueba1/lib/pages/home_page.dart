62965fa3e3b07758f85411a62988319833573660
AntVill77 <antvillot77@hotmail.com>
2022-01-31 17:14:38 -0600

    container y columnas

--- flutter_pueba1/lib/pages/home_page.dart
+++ flutter_pueba1/lib/pages/home_page.dart
@@ -4,27 +4,27 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-          child: Container(
-        color: Colors.grey.withOpacity(0.4),
-        child: Row(
-          mainAxisSize: MainAxisSize.max,
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Container(
-              width: 80,
-              height: 40,
-              color: Colors.red,
-            ),
-            Container(
-              width: 40,
-              height: 80,
-              color: Colors.blue,
-            )
-          ],
-        ),
-      )),
+      body: Container(
+        color: Colors.black26,
+        width: 300,
+        height: 400,
+        child: Column(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(
+                width: 40,
+                height: 40,
+                color: Colors.red,
+              ),
+              Container(
+                width: 40,
+                height: 40,
+                color: Colors.blue,
+              )
+            ]),
+      ),
     );
   }
 }
