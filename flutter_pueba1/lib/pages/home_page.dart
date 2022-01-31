f908c937ef85fd7566711083fc18d2949af81760
AntVill77 <antvillot77@hotmail.com>
2022-01-31 17:37:27 -0600

    safe Area widget

--- flutter_pueba1/lib/pages/home_page.dart
+++ flutter_pueba1/lib/pages/home_page.dart
@@ -4,26 +4,30 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        color: Colors.black26,
-        width: 300,
-        height: 400,
-        child: Column(
-            mainAxisSize: MainAxisSize.min,
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Container(
-                width: 40,
-                height: 40,
-                color: Colors.red,
-              ),
-              Container(
-                width: 40,
-                height: 40,
-                color: Colors.blue,
-              )
-            ]),
+      body: SafeArea(
+        top: false,
+        child: Container(
+          color: Colors.black26,
+          width: 300,
+          height: 300,
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                SafeArea(
+                  child: Container(
+                    width: 40,
+                    height: 40,
+                    color: Colors.red,
+                  ),
+                ),
+                Container(
+                  width: 40,
+                  height: 40,
+                  color: Colors.blue,
+                )
+              ]),
+        ),
       ),
     );
   }
