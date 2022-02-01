0ea0ffe057b390dda59818b02a6823b434f418c8
AntVill77 <antvillot77@hotmail.com>
2022-01-31 21:04:49 -0600

    pruebas stack

--- flutter_pueba1/lib/pages/home_page.dart
+++ flutter_pueba1/lib/pages/home_page.dart
@@ -1,34 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        top: false,
-        child: Container(
-          color: Colors.black26,
-          width: 300,
-          height: 300,
-          child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                SafeArea(
-                  child: Container(
-                    width: 40,
-                    height: 40,
-                    color: Colors.red,
-                  ),
-                ),
-                Container(
-                  width: 40,
-                  height: 40,
-                  color: Colors.blue,
-                )
-              ]),
-        ),
+        body: Container(
+      width: double.infinity,
+      height: double.infinity,
+      color: Colors.grey,
+      child: Stack(
+        alignment: Alignment.center,
+        children: [
+          Container(
+            width: 100,
+            height: 100,
+            color: Colors.red,
+          ),
+          Container(
+            width: 60,
+            height: 60,
+            color: Colors.blue,
+          ),
+          Text(
+            "hello",
+          ),
+        ],
       ),
-    );
+    ));
   }
 }
